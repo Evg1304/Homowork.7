112f9a3b010501faeee81b32b5699491f1e2aff6
mishe <mishel1400@mail.ru>
2022-08-30 21:06:38 +0300

    Выполнила ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,62 @@
-public class Main {
-    public static void main(String[] args) {
+ public class Arrays {
+     public static int[] generateRandomArray() {
 
-        System.out.println("Hello world!");
-    // Задание 1
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println( "ФИО сотрудника- "+fullName);
-        //Задание 2
-        String FullName = "Ivanov Ivan Ivanovich";
-        String upperFullName = FullName.toUpperCase();
-        System.out.println("Данные сотрудника для заполнения отчета-" + upperFullName);
-        //Задание 3
-        String fullNamee = "Иванов Семён Семёнович";
-        System.out.println("Данные ФИО сотрудника - "+ fullNamee.replace("ё","е"));
-    }
-}
+         java.util.Random random = new java.util.Random();
+         int[] arr = new int[30];
+         for (int i = 0; i < arr.length; i++) {
+             arr[i] = random.nextInt(100_000) + 100_000;
+         }
+         return arr;
+     }
+
+     public static void mail(String[] args) {
+         var salaries = generateRandomArray();
+         int sum = 0;
+         for (int i = 0; i < salaries.length; i++) {
+             sum += salaries[i];
+         }
+         System.out.printf("Сумма трат за месяц составила " + sum + " рублей");
+         System.out.println(salaries);
+     }
+
+     public static void task2() {
+         var salaries = generateRandomArray();
+         int min = salaries[0];
+         int max = salaries[0];
+         for (var salary : salaries) {
+             if (max < salary) {
+                 max = salary;
+             }
+             if (min > salary) {
+                 min = salary;
+             }
+
+         }
+         System.out.printf("Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + "  рублей");
+     }
+
+     public static void task3() {
+         var salaries = generateRandomArray();
+         int sum = 0;
+         for (var salary : salaries) {
+             sum += salary;
+
+         }
+         System.out.println("Средняя сумма трат за месяц составила "+sum/ salaries.length+" рублей");
+     }
+     public static void task4(){
+         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+         for (int i = reverseFullName.length-1 ; i >= 0; i--) {
+             System.out.println(reverseFullName[i]);
+         }
+     }
+ }
+
+
+
+
+
+
+
+
+
